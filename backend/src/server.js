bf0b03f807eaea63dde633f0b8ef14013dc851a3
sanskar321 <200333sanskar@cosmoscollege.edu.np>
2025-07-25 12:30:58 +0545

    scripted commit from backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost",
     credentials: true, // allow frontend to send cookies
   })
 );
